5a0341da4371f4064a9386e7c9874da12532a1b4
Wahaj Al Obid <whalobid@student.rtc.edu>
2025-03-06 13:55:52 -0800

    Update code.py

---
+++ code.py
@@ -0,0 +1,16 @@
+# CNE 340 Final Project
+#Project Name: Historic Gold Price Data Analysis
+# Due date: 03/18/2025
+# Project Team : 1- Wahaj AL Obid, 2- Aaron Henson, 3- Lidsyda Nouanphachan
+import pandas as pd
+import numpy as np
+import mysql.connector
+import requests
+import time
+
+def connect_to_sql():
+    conn = mysql.connector.connect(user='root', password='',
+                                  host='127.0.0.1',
+                                  database='cne340_fp')
+    return conn
+
