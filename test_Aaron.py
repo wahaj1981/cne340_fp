64f58825b3da0a7161500ecbdb7b0bbecaa8128d
Aaron Henson <182049784+Hensonam23@users.noreply.github.com>
2025-03-11 22:44:08 -0700

    Delete code that is not needed

--- test_Aaron.py
+++ test_Aaron.py
@@ -113,19 +113,6 @@ df.set_index('Date', inplace=True)
 yearly_avg = df['Gold_Price'].resample('YE').mean()
 print(yearly_avg)
 
-# Create a sample DataFrame with different types of data
-df2 = pd.DataFrame(
-    {
-        "A": 1.0,
-        "B": pd.Timestamp("20130102"),
-        "C": pd.Series(1, index=list(range(4)), dtype="float32"),
-        "D": np.array([3] * 4, dtype="int32"),
-        "E": pd.Categorical(["test", "train", "test", "train"]),
-        "F": "foo",
-    }
-)
-print(df2)
-
 ########################################################################################
 
 # Plot 1: Line chart showing gold price over time
